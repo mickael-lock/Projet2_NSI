af87810a8fe21da35e7c249e4b7ea65e7973d77d
mickael-lock <95338910+mickael-lock@users.noreply.github.com>
2021-12-06 14:31:43 +0100

    Update classe_Mouton.py

--- classe_Mouton.py
+++ classe_Mouton.py
@@ -1,2 +1,11 @@
-class Mouton(gain_nourriture, position_x and position_y, energie, taux_reproduction):
+class Mouton(gain_nourriture, position_x,position_y, energie, taux_reproduction):
+ """
+ gain_nourriture: gain énergie apporté par la conso d'un carré d'herbe / valeur par défaut = 4
+ position_x et position_y: position du mouton couple de x;y
+ energie: >0 ou =0 / si energie = 0 le mouton meurt + initialisé entre 1 et 2 X gain_nourriture
+ taux_reproduction: entier entre 1 et 100, 4 par défaut, chance d'un mouton de se reproduire
+ """
  
+ def __init__(self):
+  self.mouton = self
+ def variationEnergie(i,j):
